8153becafaa0f934a77450638a348adb22883013
João Birelo Neto <njoao6016@gmail.com>
2022-03-01 21:38:41 -0300

    :sparkles: created methods on MongoConnection

--- backend/src/db/MongoConnection.js
+++ backend/src/db/MongoConnection.js
@@ -1,6 +1,48 @@
-module.exports = class MongoConnection {
-  static #clientMongo = null;
+const { MongoClient, ReadPreference } = require('mongodb')
 
-  static async db() { }
-  static async close() { }
+module.exports = class MongoConnection {
+  static #clientMongo = null
+
+  static getStringURI(env) {
+    const enviroments = {
+      dev() { return process.env.MONGO_STRINGURI_DEV },
+      test() { return process.env.MONGO_STRINGURI_TEST },
+      prod() { return process.env.MONGO_STRINGURI_PROD },
+    }
+
+    return enviroments[env]()
+  }
+
+  static async db() {
+    try {
+      const stringURI = this.getStringURI(process.env.ENVIRONMENT)
+      const params = 'keepAlive=true&socketTimeoutMS=5000&connectTimeoutMS=5000'
+
+      this.#clientMongo = new MongoClient(`${stringURI}?${params}`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        readPreference: ReadPreference.PRIMARY_PREFERRED
+      })
+
+      const connection = await this.#clientMongo.connect();
+
+      const dbName = `${process.env.MONGO_DBNAME}_${process.env.ENVIRONMENT}`
+      const db = connection.db(dbName)
+
+      return db
+    } catch (error) {
+      console.log(error)
+      return false
+    }
+  }
+
+  static async close() {
+    try {
+      if (this.#clientMongo) this.#clientMongo.close()
+      return true;
+    } catch (error) {
+      console.log(error)
+      return false
+    }
+  }
 }
