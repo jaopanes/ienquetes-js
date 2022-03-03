686c5111c0f983aea642b4ab8c8c61a49383fdbb
João Birelo Neto <njoao6016@gmail.com>
2022-03-02 22:08:31 -0300

    :sparkles: added field deleteat at on Survey

--- backend/src/models/Survey.js
+++ backend/src/models/Survey.js
@@ -13,6 +13,7 @@ module.exports = class Survey {
    * @param {array} param.options Options survey.
    * @param {string} [param.createdAt] Created at date (YYYY-MM-DD HH:mm:ss).
    * @param {string} [param.updatedAt] Updated at date (YYYY-MM-DD HH:mm:ss).
+   * @param {string} [param.deletedAt] Deleted at date (YYYY-MM-DD HH:mm:ss).
    * @param {string} [param.id] Id (UUID).
    */
   constructor({
@@ -22,6 +23,7 @@ module.exports = class Survey {
     options,
     createdAt = date.currentDateHour(),
     updatedAt = date.currentDateHour(),
+    deletedAt = null,
     id = uuid.generate()
   }) {
     this.title = title
@@ -30,6 +32,7 @@ module.exports = class Survey {
     this.options = options
     this.createdAt = createdAt
     this.updatedAt = updatedAt
+    this.deletedAt = deletedAt
     this.id = id
   }
 }
