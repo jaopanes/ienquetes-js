f92a85b3efca283dc369e34b0f8f9736c1755e3b
João Birelo Neto <njoao6016@gmail.com>
2022-03-05 12:47:49 -0300

    :sparkles: added sass and router to frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
+import router from './router'
+
+import './assets/scss/_reset.scss'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Página inicial</h1>
+</template>
+
+<script>
+export default {
+  name: "Home"
+}
+</script>
+
+<style lang="scss">
+</style>
